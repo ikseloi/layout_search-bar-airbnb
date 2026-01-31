dd62484576a09e2d262788ee1d8b3bad741c239b
Alex <gdalexgo@gmail.com>
2026-01-31 17:27:03 +0200

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,134 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document_1024px.png",
+        "test": "..\\bitmaps_test\\20260131-172453\\Entire_document_1024px.png",
+        "selector": "document",
+        "fileName": "Entire_document_1024px.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "1024px",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.5706787109375,
+          "misMatchPercentage": "0.57",
+          "analysisTime": 21
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document_720.png",
+        "test": "..\\bitmaps_test\\20260131-172453\\Entire_document_720.png",
+        "selector": "document",
+        "fileName": "Entire_document_720.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "720",
+        "error": "Reference file not found F:\\projects\\mate-academy\\02-html-css-advanced\\03-bem\\layout_search-bar-airbnb\\backstop_data\\bitmaps_reference\\Entire_document_720.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Big_search_bar_data-qa_big_1024px.png",
+        "test": "..\\bitmaps_test\\20260131-172453\\Big_search_bar_data-qa_big_1024px.png",
+        "selector": "[data-qa=\"big\"]",
+        "fileName": "Big_search_bar_data-qa_big_1024px.png",
+        "label": "Big search bar data-qa_big",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "1024px",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.768140589569161,
+          "misMatchPercentage": "0.77",
+          "analysisTime": 10
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Big_search_bar_data-qa_big_720.png",
+        "test": "..\\bitmaps_test\\20260131-172453\\Big_search_bar_data-qa_big_720.png",
+        "selector": "[data-qa=\"big\"]",
+        "fileName": "Big_search_bar_data-qa_big_720.png",
+        "label": "Big search bar data-qa_big",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "720",
+        "error": "Reference file not found F:\\projects\\mate-academy\\02-html-css-advanced\\03-bem\\layout_search-bar-airbnb\\backstop_data\\bitmaps_reference\\Big_search_bar_data-qa_big_720.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Small_search_bar_data-qa_small_1024px.png",
+        "test": "..\\bitmaps_test\\20260131-172453\\Small_search_bar_data-qa_small_1024px.png",
+        "selector": "[data-qa=\"small\"]",
+        "fileName": "Small_search_bar_data-qa_small_1024px.png",
+        "label": "Small search bar data-qa_small",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "1024px",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.9282879818594105,
+          "misMatchPercentage": "0.93",
+          "analysisTime": 9
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Small_search_bar_data-qa_small_720.png",
+        "test": "..\\bitmaps_test\\20260131-172453\\Small_search_bar_data-qa_small_720.png",
+        "selector": "[data-qa=\"small\"]",
+        "fileName": "Small_search_bar_data-qa_small_720.png",
+        "label": "Small search bar data-qa_small",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "720",
+        "error": "Reference file not found F:\\projects\\mate-academy\\02-html-css-advanced\\03-bem\\layout_search-bar-airbnb\\backstop_data\\bitmaps_reference\\Small_search_bar_data-qa_small_720.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
